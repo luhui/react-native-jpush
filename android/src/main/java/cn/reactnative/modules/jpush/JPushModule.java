17def62a5e17b02172ef43e0e4014cd8c4db4f21
陆晖 <luhui@cvte.cn>
2016-10-26 14:44:31 +0800

    [bugfix]收到jpush的自定义消息时不再进行转发

--- android/src/main/java/cn/reactnative/modules/jpush/JPushModule.java
+++ android/src/main/java/cn/reactnative/modules/jpush/JPushModule.java
@@ -37,14 +37,14 @@ import cn.jpush.android.data.JPushLocalNotification;
  * Created by lvbingru on 10/23/15.
  */
 public class JPushModule extends ReactContextBaseJavaModule {
-
+    
     private static Boolean registered = false;
     private static JPushModule gModules = null;
     private static String holdMessage = null;
-
+    
     public JPushModule(ReactApplicationContext reactContext) {
         super(reactContext);
-
+        
         if (!registered) {
             JPushInterface.init(reactContext);
             if (BuildConfig.DEBUG) {
@@ -53,12 +53,12 @@ public class JPushModule extends ReactContextBaseJavaModule {
             registered = true;
         }
     }
-
+    
     @Override
     public String getName() {
         return "JPush";
     }
-
+    
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
@@ -67,19 +67,19 @@ public class JPushModule extends ReactContextBaseJavaModule {
         }
         return constants;
     }
-
+    
     @Override
     public void initialize() {
         super.initialize();
         gModules = this;
     }
-
+    
     @Override
     public void onCatalystInstanceDestroy() {
         super.onCatalystInstanceDestroy();
         gModules = null;
     }
-
+    
     private static void sendEvent(String eventName, Bundle bundle) {
         if (gModules != null){
             WritableMap message = Arguments.fromBundle(bundle);
@@ -88,50 +88,24 @@ public class JPushModule extends ReactContextBaseJavaModule {
             return;
         }
     }
-
+    
     private static final String TAG = "JPushReceiver";
     public static void onReceive(Context context, Intent intent) {
-
-//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        
+        //        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Bundle bundle = intent.getExtras();
-
+        
         if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
             Log.d(TAG, "接受到推送下来的自定义消息");
             JPushModule.sendEvent("kJPFNetworkDidReceiveCustomMessageNotification", bundle);
-
-            String title = bundle.getString(JPushInterface.EXTRA_TITLE);
-            String msg = bundle.getString(JPushInterface.EXTRA_MESSAGE);
-            String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
-            long ID = bundle.getLong(JPushInterface.EXTRA_NOTIFICATION_ID);
-
-            if (title == null) {
-                title = "";
-            }
-            if (msg == null) {
-                msg = "";
-            }
-            if (extras == null) {
-                extras = "";
-            }
-
-            JPushLocalNotification ln = new JPushLocalNotification();
-            ln.setBuilderId(0);
-            ln.setContent(msg);
-            ln.setTitle(title);
-            ln.setNotificationId(ID);
-            ln.setExtras(extras);
-            ln.setNotificationId(System.currentTimeMillis());
-
-            JPushInterface.addLocalNotification(context.getApplicationContext(), ln);
-
         } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
             Log.d(TAG, "接受到推送下来的通知");
             JPushModule.sendEvent("kJPFNetworkDidReceiveMessageNotification", bundle);
-
+            
         } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
             Log.d(TAG, "用户点击打开了通知");
             JPushModule.sendEvent("kJPFNetworkDidOpenMessageNotification", bundle);
-
+            
             if (gModules != null && gModules.getCurrentActivity()!=null) {
                 Intent mIntent = new Intent(context, gModules.getCurrentActivity().getClass());
                 mIntent.putExtras(bundle);
@@ -142,17 +116,17 @@ public class JPushModule extends ReactContextBaseJavaModule {
             Log.d(TAG, "Unhandled intent - " + intent.getAction());
         }
     }
-
+    
     @ReactMethod
     public void stopPush(){
         JPushInterface.stopPush(getReactApplicationContext());
     }
-
+    
     @ReactMethod
     public void resumePush(){
         JPushInterface.resumePush(getReactApplicationContext());
     }
-
+    
     @ReactMethod
     public void setAlias(String alias) {
         JPushInterface.setAlias(getReactApplicationContext(), alias, null);
@@ -161,44 +135,44 @@ public class JPushModule extends ReactContextBaseJavaModule {
     public void setTags(ReadableArray tags, String alias) {
         JPushInterface.setAliasAndTags(getReactApplicationContext(), alias, _stringArrayToSet(tags));
     }
-
+    
     @ReactMethod
     public void getRegistrationID(Callback callback) {
         String registrationID = JPushInterface.getRegistrationID(getReactApplicationContext());
         callback.invoke(registrationID == null ? "" : registrationID);
     }
-
+    
     @ReactMethod
     public void clearLocalNotifications() {
         JPushInterface.clearLocalNotifications(getReactApplicationContext());
     }
-
+    
     @ReactMethod
     public void clearAllNotifications() {
         JPushInterface.clearAllNotifications(getReactApplicationContext());
     }
-
+    
     @ReactMethod
     public void clearNotificationById(int notificationId) {
         JPushInterface.clearNotificationById(getReactApplicationContext(), notificationId);
     }
-
+    
     @ReactMethod
     public void setPushTime
-            (ReadableArray weaks, int startHour, int endHour) {
+    (ReadableArray weaks, int startHour, int endHour) {
         JPushInterface.setPushTime(getReactApplicationContext(), _intArrayToSet(weaks), startHour, endHour);
     }
-
+    
     @ReactMethod
     public void setSilenceTime(int startHour, int startMinute, int endHour, int endMinute) {
         JPushInterface.setSilenceTime(getReactApplicationContext(), startHour, startMinute, endHour, endMinute);
     }
-
+    
     @ReactMethod
     public void setLatestNotificationNumber(int maxNum) {
         JPushInterface.setLatestNotificationNumber(getReactApplicationContext(), maxNum);
     }
-
+    
     private Set _stringArrayToSet(ReadableArray array) {
         Set<String> set = new HashSet<String>();
         if (array != null) {
@@ -221,27 +195,27 @@ public class JPushModule extends ReactContextBaseJavaModule {
         }
         return set;
     }
-
+    
     public static class JPushReceiver extends BroadcastReceiver {
-
+        
         public JPushReceiver() {}
-
+        
         @Override
         public void onReceive(Context context, Intent intent) {
-
-//            Log.e(TAG, "onReceive - " + intent.getAction());
-//            Log.e(TAG, "onReceive - " + intent.getExtras().toString());
-
+            
+            //            Log.e(TAG, "onReceive - " + intent.getAction());
+            //            Log.e(TAG, "onReceive - " + intent.getExtras().toString());
+            
             boolean isAppRunning = _isApplicationRunning(context);
-//            Log.e("onReceive", isAppRunning ? "running" : "not running");
-
+            //            Log.e("onReceive", isAppRunning ? "running" : "not running");
+            
             if (isAppRunning) {
                 JPushModule.onReceive(context, intent);
             }
             else {
                 if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
                     Bundle bundle = intent.getExtras();
-
+                    
                     JSONObject json = new JSONObject();
                     Set<String> keys = bundle.keySet();
                     for (String key : keys) {
@@ -251,7 +225,7 @@ public class JPushModule extends ReactContextBaseJavaModule {
                         }
                     }
                     JPushModule.holdMessage = json.toString();
-
+                    
                     String packageName = context.getApplicationContext().getPackageName();
                     Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
                     launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
@@ -259,7 +233,7 @@ public class JPushModule extends ReactContextBaseJavaModule {
                 }
             }
         }
-
+        
         private boolean _isApplicationRunning(Context context) {
             ActivityManager activityManager = (ActivityManager) context.getApplicationContext().getSystemService(context.ACTIVITY_SERVICE);
             List<ActivityManager.RunningAppProcessInfo> processInfos = activityManager.getRunningAppProcesses();
